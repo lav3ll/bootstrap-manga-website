7952b46583279a950101d41dabca2a47aef2e89c
lav3ll <lavell.francis@hotmail.com>
2024-02-22 13:39:36 +0000

    fixed popular img sizes so they are all uniform

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -9,11 +9,21 @@ const Popular = () => {
     <div className='row col-md-7 offset-md-2 my-4 bg-secondary rounded'>
       <p className='fw-semibold ms-2 my-3 text-white '>Popular</p>
       <div className='row justify-content-center'>
-        <div className='card col-2 mx-2'>
-          <img src='' alt='test img' className='poularImg' />
-          <p className='popularTitle'>Manga Title</p>
-          <p className='popularChapNum'>Chapter Number</p>
-        </div>
+        {reducedPopularData.map((popularManga) => (
+          <div
+            className='card col-2 mx-2 px-0 bg-transparent'
+            key={popularManga.id}
+          >
+            <img
+              src={popularManga.thumbnail_url}
+              alt={`thumbnail image of ${popularManga.title}`}
+              className='poularImg'
+              style={{ height: '200px', objectFit: 'cover' }}
+            />
+            <p className='popularTitle'>{popularManga.title}</p>
+            <p className='popularChapNum'>{popularManga.latest_chapter}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
