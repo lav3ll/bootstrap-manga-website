377f4f5ba09e650ec7de47c0a2c87eaa1404876d
lav3ll <lavell.francis@hotmail.com>
2024-02-22 14:23:20 +0000

    fixed popular text spacing

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -4,14 +4,14 @@ import popularData from '../../Data/populatTest.json';
 
 const Popular = () => {
   const reducedPopularData = popularData.data.splice(0, 5);
-  console.log(reducedPopularData);
+  // console.log(reducedPopularData);
   return (
     <div className='row col-md-7 offset-md-2 my-4 bg-secondary rounded'>
-      <p className='fw-semibold ms-2 my-3 text-white '>Popular</p>
+      <p className='fw-semibold ms-2 my-3 text-white'>Popular</p>
       <div className='row justify-content-center'>
         {reducedPopularData.map((popularManga) => (
           <div
-            className='card col-2 mx-2 px-0 bg-transparent'
+            className='card col-2 mx-2 px-0 bg-transparent border-0'
             key={popularManga.id}
           >
             <img
@@ -20,8 +20,17 @@ const Popular = () => {
               className='poularImg'
               style={{ height: '200px', objectFit: 'cover' }}
             />
-            <p className='popularTitle'>{popularManga.title}</p>
-            <p className='popularChapNum'>{popularManga.latest_chapter}</p>
+            <div className='popularTxt pt-2 fw-semibold'>
+              <p
+                className='popularTitle text-white'
+                style={{ height: '60px', overflow: 'hidden' }}
+              >
+                {popularManga.title.slice(0, 40)}
+              </p>
+              <p className='popularChapNum text-gray pt-0'>
+                Chapter {popularManga.latest_chapter}
+              </p>
+            </div>
           </div>
         ))}
       </div>
