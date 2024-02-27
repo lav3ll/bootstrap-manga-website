32d92b1ba2604639cdc52012d7d9b763a6410209
lav3ll <lavell.francis@hotmail.com>
2024-02-27 15:48:33 +0000

    latest bnse template completed

--- src/Components/Latest/LatestContainer.jsx
+++ src/Components/Latest/LatestContainer.jsx
@@ -6,8 +6,8 @@ const LatestContainer = () => {
   // console.log(latestData);
   return (
     <div className='row col-lg-7 offset-lg-2 col-sm-1 col-md-12 my-4 bg-secondary rounded gx-3 justify-content-center'>
-      <p className='fw-semibold ms-2 my-3 text-white'>latest</p>
-      <div className='row justify-content-center'>
+      <p className='fw-semibold ms-2 my-3 text-white'>Latest</p>
+      <div className='row'>
         {latestData.data.map((latestManga, idx) => (
           <Latest key={latestManga.id} latestManga={latestManga} idx={idx} />
         ))}
