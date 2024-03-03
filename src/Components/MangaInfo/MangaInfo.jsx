18c6af32ead3eb6dc202e6055c9178638bbec7aa
lav3ll <lavell.francis@hotmail.com>
2024-03-03 19:09:15 +0000

    installed react-icons

--- src/Components/MangaInfo/MangaInfo.jsx
+++ src/Components/MangaInfo/MangaInfo.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdOutlineArrowRight } from 'react-icons/md';
 
 const MangaInfo = ({ MangaInfo }) => {
   return (
     <>
+      <div className='manga-info-toplinks'>
+        <Link to={{ pathname: '/' }}>EliteScans</Link> <MdOutlineArrowRight />
+        <Link to={{ pathname: '/' }}>EliteScans</Link>
+      </div>
       <div className='row manga-info-container'>
         <div className='col-3 bg-light'>TEST</div>
         <div className='col-9 bg-dark'>TEST</div>
