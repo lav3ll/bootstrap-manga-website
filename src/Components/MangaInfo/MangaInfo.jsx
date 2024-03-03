b53af11e07fdaf0b1f53084bd32b1661b96fdd17
lav3ll <lavell.francis@hotmail.com>
2024-03-03 20:17:14 +0000

    left col manga detail completed

--- src/Components/MangaInfo/MangaInfo.jsx
+++ src/Components/MangaInfo/MangaInfo.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdOutlineArrowRight } from 'react-icons/md';
 import './MangaInfo.css';
-import MangaInfo from '../../Data/getMangaTest.json';
-
+import MangaInf from '../../Data/getMangaTest.json';
+const Info = MangaInf.data;
 const MangaInfo = ({}) => {
+  const saveBookmark = () => {
+    alert('saved to local storage');
+  };
   return (
     <>
       <div className='manga-info-wrapper'>
@@ -17,15 +20,30 @@ const MangaInfo = ({}) => {
           </Link>{' '}
           <MdOutlineArrowRight />
           <Link
-            to={{ pathname: `/${MangaInfo}` }}
+            to={{ pathname: `/${Info}` }}
             style={{ color: 'white', textDecoration: 'none' }}
           >
             EliteScans
           </Link>
         </div>
         <div className='row manga-info-container row col-7 offset-1'>
-          <div className='col-5 bg-light rounded'>TEST</div>
-          <div className='bg-dark rounded col-7'>TEST</div>
+          <div className='col-5 bg-light rounded row text-center'>
+            <img src={Info.thumb} alt='test' />
+            <button
+              className='btn custom-bg-secondary mt-2 text-white fw-semibold'
+              onClick={saveBookmark}
+            >
+              {' '}
+              Bookmark
+            </button>
+            <p className='status my-1 fw-semibold bg-secondary rounded py-1'>
+              {Info.status === '' ? 'Unknown' : Info.status}
+            </p>
+            <p className='type my-1 fw-semibold bg-secondary rounded py-1'>
+              {Info.type}
+            </p>
+          </div>
+          <div className='bg-dark rounded ms-4 col-7'>TEST</div>
         </div>
       </div>
     </>
