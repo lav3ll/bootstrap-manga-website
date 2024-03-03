25bdf4f3e15528e235102515af44def7d4e38c1d
lav3ll <lavell.francis@hotmail.com>
2024-03-03 20:59:38 +0000

    added manga-detail genre buttons

--- src/Components/MangaInfo/MangaInfo.jsx
+++ src/Components/MangaInfo/MangaInfo.jsx
@@ -8,6 +8,10 @@ const MangaInfo = ({}) => {
   const saveBookmark = () => {
     alert('saved to local storage');
   };
+
+  const genreSearch = () => {
+    alert('search for genre');
+  };
   return (
     <>
       <div className='manga-info-wrapper'>
@@ -43,7 +47,24 @@ const MangaInfo = ({}) => {
               {Info.type}
             </p>
           </div>
-          <div className='bg-dark rounded ms-4 col-7'>TEST</div>
+          <div className='bg-dark rounded ms-4 col-7'>
+            <div className='info-mid-topcol'>
+              <h2 className='text-white mx-2 my-3'>{Info.title}</h2>
+              <p className='text-white mx-2'>Synopsis {Info.title}</p>
+              <p className='text-white mx-2'>{Info.summary.slice(0, 282)}</p>
+            </div>
+            <div className='info-mid-botcol'>
+              {Info.genres.map((genre, index) => (
+                <button
+                  key={index}
+                  className='btn text-white fs-6 px-1'
+                  onClick={genreSearch}
+                >
+                  {genre}
+                </button>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </>
