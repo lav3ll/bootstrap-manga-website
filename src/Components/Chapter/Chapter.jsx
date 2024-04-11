3bd72a59f8d42e1ae6b51468e6c95952006c537c
lav3ll <lavell.francis@hotmail.com>
2024-04-11 16:44:19 +0100

    added base layout for chapter overhead buttons

--- src/Components/Chapter/Chapter.jsx
+++ src/Components/Chapter/Chapter.jsx
@@ -7,11 +7,23 @@ const Chapter = () => {
   const { state } = useLocation();
   const { chapterImages, chapters, index } = state || {};
 
-  console.log(chapters);
+  //   console.log(chapters);
 
   return (
     <>
       <div>Chapter {id}</div>
+      <div className='chapter-links row col-8 bg-primary mx-auto'>
+        <div className='chapter-select col-3 bg-secondary'>test</div>
+        <div className='chapter-next-prev row col-5 ms-auto'>
+          <div className='btn btn-primary chapter-prev col-6 text-end'>
+            Prev
+          </div>
+          <div className='btn btn-primary chapter-next col-6 text-start'>
+            Next
+          </div>
+        </div>
+      </div>
+
       <div className='chapter-img-container bg-secondary'>
         <div className='chapter-images row col-sm-12 col-md-8 col-lg-8 mx-auto'>
           {chapterImages?.chapter?.data?.map((image, idx) => (
