ea1c54dc7ad3f538e3adffffd0aab6eb831fc04b
lav3ll <lavell.francis@hotmail.com>
2024-04-30 22:30:49 +0100

    udpated base text for bookmarks link

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Bookmarks = () => {
-  return <div>Bookmarks</div>;
+  return (
+    <div
+      className='d-flex justify-content-center align-items-center'
+      style={{ height: '100vh' }}
+    >
+      <div>
+        <h1 className='text-center'>Log in to view bookmarks</h1>
+        {/* Add login button or link here */}
+      </div>
+    </div>
+  );
 };
 
 export default Bookmarks;
