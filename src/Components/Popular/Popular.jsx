de6f628ebb38b5992c4d0daee73b9b0f53f00b3d
lav3ll <lavell.francis@hotmail.com>
2024-05-02 23:23:12 +0100

    popular div now directs to mangainfo

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,27 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './Popular.css';
+import { Link } from 'react-router-dom';
 
 const Popular = ({ popularManga, idx }) => {
-  // set hover color
   const [hoverColour, setHoverColour] = useState('text-white');
-  // set overlay
   const [overlay, setOverlay] = useState(false);
-  // set image source
   const [imageSrc, setImageSrc] = useState('');
 
-  // Update image source once popularManga is available
   useEffect(() => {
-    if (
-      popularManga &&
-      popularManga.relationships &&
-      popularManga.relationships[2]
-    ) {
-      const fileName = popularManga.relationships.find(
+    if (popularManga && popularManga.relationships) {
+      const coverArt = popularManga.relationships.find(
         (relationship) => relationship.type === 'cover_art'
-      )?.attributes.fileName;
-      const src = `https://uploads.mangadex.org/covers/${popularManga.id}/${fileName}.256.jpg`;
-      setImageSrc(src);
-      console.log(src);
+      );
+      if (coverArt && coverArt.attributes && coverArt.attributes.fileName) {
+        const fileName = coverArt.attributes.fileName;
+        const src = `https://uploads.mangadex.org/covers/${popularManga.id}/${fileName}.256.jpg`;
+        setImageSrc(src);
+      }
     }
   }, [popularManga]);
 
@@ -36,7 +31,6 @@ const Popular = ({ popularManga, idx }) => {
   };
 
   const handleClick = () => {
-    // take clicked element data and make api call for manga summary page
     alert('test');
   };
 
@@ -44,19 +38,26 @@ const Popular = ({ popularManga, idx }) => {
     <div
       className={`card ${
         idx === 4 ? 'custom-hidden' : ''
-      } custom-sm mx-lg-2 px-lg-0 mx-md-2 px-md-0 mx-sm-0 px-sm-0 bg-transparent border-0 popular-card-container col-md-2  col-lg-2`}
-      key={popularManga.id}
-      onClick={handleClick}
+      } mx-lg-2 px-lg-0 mx-md-2 px-md-0 mx-sm-0 px-sm-0 bg-transparent border-0 popular-card-container col-md-2 col-lg-2 col-4`}
     >
       {imageSrc ? (
-        <img
-          src={imageSrc}
-          alt={`thumbnail image of ${popularManga.title}`}
-          className={`popularImg rounded ${overlay}`}
-          style={{ height: '200px', objectFit: 'cover' }}
-          onMouseOver={handleHoverOver}
-          onMouseOut={handleHoverOut}
-        />
+        <Link
+          to={`/manga-info/${popularManga.id}`}
+          state={{ manga: { coverImg: imageSrc, info: popularManga } }}
+        >
+          <img
+            src={imageSrc}
+            alt={`thumbnail image of ${
+              popularManga.attributes
+                ? popularManga.attributes.title.en
+                : 'Loading...'
+            }`}
+            className={`popularImg rounded ${overlay}`}
+            style={{ height: '200px', objectFit: 'cover' }}
+            onMouseOver={handleHoverOver}
+            onMouseOut={handleHoverOut}
+          />
+        </Link>
       ) : (
         <div>Loading...</div>
       )}
