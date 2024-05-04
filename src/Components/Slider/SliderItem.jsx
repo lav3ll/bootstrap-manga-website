1d934be79f35a55f5f01027dd67d07be02624b0e
lav3ll <lavell.francis@hotmail.com>
2024-05-04 22:36:13 +0100

    fixed blurry eges of image slides

--- src/Components/Slider/SliderItem.jsx
+++ src/Components/Slider/SliderItem.jsx
@@ -12,20 +12,49 @@ const SliderItem = ({ imageData }) => {
     : imageData.attributes.title[0];
   return (
     <div className='slide-container w-100'>
-      <div className='mangaSlideInfo'>
-        <p className=''></p>
-      </div>
-      <img
-        src={src}
-        alt={`${imageData.title} slider background image`}
-        className='w-100 slide-img'
-      />
-      <div className='position-absolute top-50 end-0 translate-middle-y row'>
-        <img
-          className=' rounded rounded-4 slide-img-sm ms-auto me-5'
-          src={src}
-          alt={`${imageData.title} slider image`}
-        />
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url("${src}")`,
+          backgroundSize: 'cover',
+          height: '100%',
+          backgroundPosition: 'center',
+          position: 'absolute',
+          filter: 'blur(3px)',
+          WebkitFilter: 'blur(2px)',
+          zIndex: -1,
+        }}
+      ></div>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url("${src}")`,
+          backgroundSize: 'cover',
+          height: '100%',
+          backgroundPosition: 'center',
+          filter: 'blur(5px)',
+          WebkitFilter: 'blur(8px)',
+          position: 'absolute',
+          zIndex: -1,
+        }}
+      ></div>
+
+      <div>
+        <div className='mangaSlideInfo'>
+          <p className=''>title</p>
+          <p className=''>genre</p>
+          <p className=''>summary</p>
+          <p className=''>status</p>
+          <p className=''>author</p>
+        </div>
+
+        <div className='position-absolute top-50 end-0 translate-middle-y row'>
+          <img
+            className=' rounded rounded-4 slide-img-sm ms-auto me-5'
+            src={src}
+            alt={`${imageData.title} slider image`}
+          />
+        </div>
       </div>
     </div>
   );
