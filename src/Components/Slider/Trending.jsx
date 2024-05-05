d7cbf3b3f7a6911b0e1dd2bfe0823f17cb350975
lav3ll <lavell.francis@hotmail.com>
2024-05-05 10:50:49 +0100

    added an overlay to trending image so the writingis easier to read

--- src/Components/Slider/Trending.jsx
+++ src/Components/Slider/Trending.jsx
@@ -51,17 +51,17 @@ const Trending = () => {
           <div
             className='mx-2 rounded'
             style={{
-              backgroundImage: `url(${coverImage})`,
+              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("${coverImage}")`,
               backgroundSize: 'cover',
               backgroundPosition: 'center',
               width: '100%',
               height: '250px',
             }}
           >
-            <p className='fw-semibold lg-fs-1 md-fw-4 md-fs-4 text-white d-flex align-items-center justify-content-center pt-5 md-px-2 lg-px-2 trending-week'>
+            <p className='px-2 fw-semibold lg-fs-1 md-fw-4 md-fs-4 text-white d-flex align-items-center justify-content-center pt-5 md-px-2 lg-px-2 trending-week'>
               ELITE SCANS TRENDING THIS WEEK!
             </p>
-            <p className='custom-primary pt-2 mt-2 px-4 fw-bold lg-fs-1 md-fw-4 md-fs-4'>
+            <p className='text-white pt-2 mt-2 px-4 fw-bold lg-fs-1 md-fw-4 md-fs-4'>
               {trendingManga.attributes.title.en}
             </p>
           </div>
